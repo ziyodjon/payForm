aba7fd6c42cd79429b5fd68f4a419462c0d28600
ziyodjon <ziyodjon@gmail.com>
2024-06-07 11:13:08 +0500

    LastChanges

--- main.js
+++ main.js
@@ -2,19 +2,20 @@ import { el, mount } from "redom";
 
 const app = document.getElementById('app');
 
-const formCaption = el('h1',{class:'form-caption'},'Форма для заполнения данных карты');
-const form = el("form",{id:'payform',action:''}, 
+const formCaption = el('h1.form-caption','Форма для заполнения данных карты');
+
+const form = el("form#payform",{action:''}, 
   [
-    el("label",{class:'input-label'}, 'Номер карты'), 
+    el("label.input-label", 'Номер карты'), 
     el("input",{type:'text',placeholder:'Номер карты'}),
 
-    el("label",{class:'input-label'}, 'Дата окончания действия карты'), 
+    el("label.input-label", 'Дата окончания действия карты'), 
     el("input",{type:'text',placeholder:'Дата окончания действия карты'}),
 
-    el("label",{class:'input-label'}, 'CVV/CVC'), 
+    el("label.input-label", 'CVV/CVC'), 
     el("input",{type:'text',placeholder:'CVV/CVC'}),
     
-    el("label",{class:'input-label'}, 'E-mail'), 
+    el("label.input-label", 'E-mail'), 
     el("input",{type:'text',placeholder:'E-mail'}),
 
     el("button.btn.submit-btn.disabled",{type:'submit', disabled:"disabled"},'Оплатить'),
